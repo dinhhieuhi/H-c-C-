ad5b5f9dcfcc3a45a4febf104bd5485b57a735fc
dinhhieuhi <119572240+dinhhieuhi@users.noreply.github.com>
2022-12-01 16:05:34 +0700

    Add files via upload

---
+++ c++.vhieudz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<100 + 500<<endl;
+	
+	float a,b,c;
+	a = 900;
+	b = 300;
+	c = 1000;
+	cout<<"Tong 3 so = "<<a + b + c<<endl;
+	cout<<"Hieu 3 so = "<<a - b - c<<endl;
+	cout<<"Tich 3 so ="<<a * b * c<<endl;
+	cout<<"Thuong 3 so ="<<a / b / c<<endl;
+	
+	return 0;
+}
